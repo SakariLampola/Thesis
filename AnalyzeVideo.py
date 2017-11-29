0a95dba122210b5ff97f9e1fe0356795e2726971
SakariLampola <34031485+SakariLampola@users.noreply.github.com>
2017-11-29 14:59:16 +0200

    20171129
    AnalyzeVideo - video handling included

--- AnalyzeVideo.py
+++ AnalyzeVideo.py
@@ -9,6 +9,20 @@ Created on Wed Nov 29 13:44:02 2017
 import cv2
 import ImageObjectDetection as iod
 
+cap = cv2.VideoCapture("videos/CarsOnHighway001.mp4")
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    detected_objects = iod.detect_objects(frame, 0.2)
+    for detected_object in detected_objects:
+        detected_object.print_attributes()
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
 image = cv2.imread("images/example_01.jpg")
 detected_objects = iod.detect_objects(image, 0.2)
 for detected_object in detected_objects:
