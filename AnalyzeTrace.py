ed02c34225bd07185f3a08f99b8c46a9ab216999
SakariLampola <34031485+SakariLampola@users.noreply.github.com>
2018-01-08 16:10:10 +0200

    20180108
    Presentation, Jupyter Notebook

--- AnalyzeTrace.py
+++ AnalyzeTrace.py
@@ -31,5 +31,18 @@ def detected_object_hue_distribution(id):
     dd8 = pd.DataFrame({'Hue': 'f8', 'Fraction' :object_data['do_c8']})
     dd = pd.concat([dd1, dd2, dd3, dd4, dd5, dd6, dd7, dd8])
     sns.boxplot(x="Hue", y="Fraction", data=dd)
+    
+def boundingbox_movement(id):
+    object_data = data[['x_min_m','x_max_m','y_min_m','y_max_m']].loc[data['id']==id]
+    object_data.plot()    
 
-detected_object_hue_distribution(161)
+def boundingbox_movement_diff(id):
+    object_data = data[['x_min_m','x_max_m','y_min_m','y_max_m']].loc[data['id']==id]
+    object_data = object_data.diff(1)
+    object_data = object_data.dropna()
+    plt.figure(1)
+    sns.pairplot(object_data)
+    plt.figure(2)
+    object_data.plot()
+
+boundingbox_movement_diff(161)
