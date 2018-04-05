03abb821f0ecee0aee02ab230d516a890c4716e4
SakariLampola <34031485+SakariLampola@users.noreply.github.com>
2018-04-05 17:06:17 +0300

    20180405
    Semantic Segmentation

--- ShadowWorldSegment.py
+++ ShadowWorldSegment.py
@@ -11,30 +11,29 @@ import sys
 from pycocotools.coco import COCO
 import numpy as np
 import cv2
-import time
-import random as rnd
-import pyttsx3
-import winsound
-from math import atan, cos, sqrt, exp, log, atan2, sin, pi
+#import time
+#import random as rnd
+#import pyttsx3
+#import winsound
+#from math import atan, cos, sqrt, exp, log, atan2, sin, pi
 from PIL import Image
-import skimage.io as io
-import matplotlib.pyplot as plt
-import pylab
+#import skimage.io as io
+#import matplotlib.pyplot as plt
+#import pylab
 import os
-import six.moves.urllib as urllib
-import sys
-import tarfile
+#import six.moves.urllib as urllib
+#import sys
+#import tarfile
 import tensorflow as tf
-import zipfile
-from collections import defaultdict
-from io import StringIO
-from PIL import Image
+#import zipfile
+#from collections import defaultdict
+#from io import StringIO
+#from PIL import Image
 # This is needed since the notebook is stored in the object_detection folder.
 sys.path.append("..")
 from object_detection.utils import ops as utils_ops
-import tensorflow as tf
 sys.path.append("..")
-import matplotlib as mpl
+#import matplotlib as mpl
 from utils import label_map_util
 from utils import visualization_utils as vis_util
 
@@ -48,6 +47,191 @@ STUFFDIR="D:\\Thesis\Coco\\annotations\\stuff_val2017_pixelmaps\\stuff_val2017_p
 NUM_CLASSES = 90
 PATH_TO_CKPT = r'D:\Thesis\Models\faster_rcnn_resnet101_coco_2018_01_28\frozen_inference_graph.pb'
 PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+CATEGORY_INDEX={
+        0:{'name':'unlabeled'},
+        1:{'name':'person'},
+        2:{'name':'bicycle'},
+        3:{'name':'car'},
+        4:{'name':'motorcycle'},
+        5:{'name':'airplane'},
+        6:{'name':'bus'},
+        7:{'name':'train'},
+        8:{'name':'truck'},
+        9:{'name':'boat'},
+        10:{'name':'traffic light'},
+        11:{'name':'fire hydrant'},
+        12:{'name':'street sign'},
+        13:{'name':'stop sign'},
+        14:{'name':'parking meter'},
+        15:{'name':'bench'},
+        16:{'name':'bird'},
+        17:{'name':'cat'},
+        18:{'name':'dog'},
+        19:{'name':'horse'},
+        20:{'name':'sheep'},
+        21:{'name':'cow'},
+        22:{'name':'elephant'},
+        23:{'name':'bear'},
+        24:{'name':'zebra'},
+        25:{'name':'giraffe'},
+        26:{'name':'hat'},
+        27:{'name':'backpack'},
+        28:{'name':'umbrella'},
+        29:{'name':'shoe'},
+        30:{'name':'eye glasses'},
+        31:{'name':'handbag'},
+        32:{'name':'tie'},
+        33:{'name':'suitcase'},
+        34:{'name':'frisbee'},
+        35:{'name':'skis'},
+        36:{'name':'snowboard'},
+        37:{'name':'sports ball'},
+        38:{'name':'kite'},
+        39:{'name':'baseball bat'},
+        40:{'name':'baseball glove'},
+        41:{'name':'skateboard'},
+        42:{'name':'surfboard'},
+        43:{'name':'tennis racket'},
+        44:{'name':'bottle'},
+        45:{'name':'plate'},
+        46:{'name':'wine glass'},
+        47:{'name':'cup'},
+        48:{'name':'fork'},
+        49:{'name':'knife'},
+        50:{'name':'spoon'},
+        51:{'name':'bowl'},
+        52:{'name':'banana'},
+        53:{'name':'apple'},
+        54:{'name':'sandwich'},
+        55:{'name':'orange'},
+        56:{'name':'broccoli'},
+        57:{'name':'carrot'},
+        58:{'name':'hot dog'},
+        59:{'name':'pizza'},
+        60:{'name':'donut'},
+        61:{'name':'cake'},
+        62:{'name':'chair'},
+        63:{'name':'couch'},
+        64:{'name':'potted plant'},
+        65:{'name':'bed'},
+        66:{'name':'mirror'},
+        67:{'name':'dining table'},
+        68:{'name':'window'},
+        69:{'name':'desk'},
+        70:{'name':'toilet'},
+        71:{'name':'door'},
+        72:{'name':'tv'},
+        73:{'name':'laptop'},
+        74:{'name':'mouse'},
+        75:{'name':'remote'},
+        76:{'name':'keyboard'},
+        77:{'name':'cell phone'},
+        78:{'name':'microwave'},
+        79:{'name':'oven'},
+        80:{'name':'toaster'},
+        81:{'name':'sink'},
+        82:{'name':'refrigerator'},
+        83:{'name':'blender'},
+        84:{'name':'book'},
+        85:{'name':'clock'},
+        86:{'name':'vase'},
+        87:{'name':'scissors'},
+        88:{'name':'teddy bear'},
+        89:{'name':'hair drier'},
+        90:{'name':'toothbrush'},
+        91:{'name':'hair brush'},
+        92:{'name':'banner'},
+        93:{'name':'blanket'},
+        94:{'name':'branch'},
+        95:{'name':'bridge'},
+        96:{'name':'building-other'},
+        97:{'name':'bush'},
+        98:{'name':'cabinet'},
+        99:{'name':'cage'},
+        100:{'name':'cardboard'},
+        101:{'name':'carpet'},
+        102:{'name':'ceiling-other'},
+        103:{'name':'ceiling-tile'},
+        104:{'name':'cloth'},
+        105:{'name':'clothes'},
+        106:{'name':'clouds'},
+        107:{'name':'counter'},
+        108:{'name':'cupboard'},
+        109:{'name':'curtain'},
+        110:{'name':'desk-stuff'},
+        111:{'name':'dirt'},
+        112:{'name':'door-stuff'},
+        113:{'name':'fence'},
+        114:{'name':'floor-marble'},
+        115:{'name':'floor-other'},
+        116:{'name':'floor-stone'},
+        117:{'name':'floor-tile'},
+        118:{'name':'floor-wood'},
+        119:{'name':'flower'},
+        120:{'name':'fog'},
+        121:{'name':'food-other'},
+        122:{'name':'fruit'},
+        123:{'name':'furniture-other'},
+        124:{'name':'grass'},
+        125:{'name':'gravel'},
+        126:{'name':'ground-other'},
+        127:{'name':'hill'},
+        128:{'name':'house'},
+        129:{'name':'leaves'},
+        130:{'name':'light'},
+        131:{'name':'mat'},
+        132:{'name':'metal'},
+        133:{'name':'mirror-stuff'},
+        134:{'name':'moss'},
+        135:{'name':'mountain'},
+        136:{'name':'mud'},
+        137:{'name':'napkin'},
+        138:{'name':'net'},
+        139:{'name':'paper'},
+        140:{'name':'pavement'},
+        141:{'name':'pillow'},
+        142:{'name':'plant-other'},
+        143:{'name':'plastic'},
+        144:{'name':'platform'},
+        145:{'name':'playingfield'},
+        146:{'name':'railing'},
+        147:{'name':'railroad'},
+        148:{'name':'river'},
+        149:{'name':'road'},
+        150:{'name':'rock'},
+        151:{'name':'roof'},
+        152:{'name':'rug'},
+        153:{'name':'salad'},
+        154:{'name':'sand'},
+        155:{'name':'sea'},
+        156:{'name':'shelf'},
+        157:{'name':'sky-other'},
+        158:{'name':'skyscraper'},
+        159:{'name':'snow'},
+        160:{'name':'solid-other'},
+        161:{'name':'stairs'},
+        162:{'name':'stone'},
+        163:{'name':'straw'},
+        164:{'name':'structural-other'},
+        165:{'name':'table'},
+        166:{'name':'tent'},
+        167:{'name':'textile-other'},
+        168:{'name':'towel'},
+        169:{'name':'tree'},
+        170:{'name':'vegetable'},
+        171:{'name':'wall-brick'},
+        172:{'name':'wall-concrete'},
+        173:{'name':'wall-other'},
+        174:{'name':'wall-panel'},
+        175:{'name':'wall-stone'},
+        176:{'name':'wall-tile'},
+        177:{'name':'wall-wood'},
+        178:{'name':'water-other'},
+        179:{'name':'waterdrops'},
+        180:{'name':'window-blind'},
+        181:{'name':'window-other'},
+        182:{'name':'wood'},
+        }
 UI_X0 = 0
 UI_X1 = 700
 UI_X2 = 1400
@@ -93,10 +277,6 @@ with detection_graph.as_default():
     serialized_graph = fid.read()
     od_graph_def.ParseFromString(serialized_graph)
     tf.import_graph_def(od_graph_def, name='')
-
-label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
-category_index = label_map_util.create_category_index(categories)
 
 def run_inference_for_single_image(image, graph):
   with graph.as_default():
@@ -145,6 +325,19 @@ def run_inference_for_single_image(image, graph):
   return output_dict
 
 
+np.random.seed(1)
+
+lut1 = np.array([np.random.randint(0,255) for i in np.arange(0, 256)]).astype("uint8")
+lut2 = np.array([np.random.randint(0,255) for i in np.arange(0, 256)]).astype("uint8")
+lut3 = np.array([np.random.randint(0,255) for i in np.arange(0, 256)]).astype("uint8")
+lut = np.dstack((lut1, lut2, lut3))
+
+np.random.seed(5)
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
 annFile='{}/annotations/instances_{}.json'.format(DATADIR,DATATYPE)
 coco=COCO(annFile)
 
@@ -152,7 +345,6 @@ annFile = '{}/annotations/captions_{}.json'.format(DATADIR,DATATYPE)
 coco_caps=COCO(annFile)
 
 imgIds = coco.getImgIds()
-np.random.seed(1)
 
 frame = np.zeros((UI_Y2+1, UI_X4+1, 3), np.uint8)
 line_color = (255,255,255)
@@ -216,7 +408,8 @@ for i in range(100):
       category_index,
       instance_masks=output_dict.get('detection_masks'),
       use_normalized_coordinates=True,
-      line_thickness=6)    
+      line_thickness=6,
+      min_score_thresh=0.01)    
     frame[left_corner_height:left_corner_height+height, UI_X1+left_corner_width:UI_X1+left_corner_width+width, 2] = image_right[:,:,0]
     frame[left_corner_height:left_corner_height+height, UI_X1+left_corner_width:UI_X1+left_corner_width+width, 1] = image_right[:,:,1]
     frame[left_corner_height:left_corner_height+height, UI_X1+left_corner_width:UI_X1+left_corner_width+width, 0] = image_right[:,:,2]
@@ -224,17 +417,90 @@ for i in range(100):
     file = STUFFDIR + img['file_name']
     file = file.replace('.jpg','.png')
     image_stuff = np.array(Image.open(file))
-    minValue = image_stuff.min()
-    maxValue = image_stuff.max()
-    disp = np.uint8(255.0 * (image_stuff - minValue) / (maxValue - minValue))
-    disp_rgb = cv2.applyColorMap(disp, cv2.COLORMAP_RAINBOW)
+    image_stuff_color = np.zeros((height, width,3), dtype='uint8')
+    image_stuff_color[:,:,0] = image_stuff[:,:]
+    image_stuff_color[:,:,1] = image_stuff[:,:]
+    image_stuff_color[:,:,2] = image_stuff[:,:]
+#    minValue = image_stuff.min()
+#    maxValue = image_stuff.max()
+#    disp = np.uint8(255.0 * (image_stuff - minValue) / (maxValue - minValue))
+#    disp_rgb = cv2.applyColorMap(disp, cv2.COLORMAP_RAINBOW)
+    disp_rgb = cv2.LUT(image_stuff_color, lut)
 
     frame[left_corner_height:left_corner_height+height, UI_X2+left_corner_width:UI_X2+left_corner_width+width, 2] = disp_rgb[:,:,0]
     frame[left_corner_height:left_corner_height+height, UI_X2+left_corner_width:UI_X2+left_corner_width+width, 1] = disp_rgb[:,:,1]
     frame[left_corner_height:left_corner_height+height, UI_X2+left_corner_width:UI_X2+left_corner_width+width, 0] = disp_rgb[:,:,2]
 
+    boxes = output_dict['detection_boxes']
+    classes = output_dict['detection_classes']
+    scores = output_dict['detection_scores']
+
+    x_rep = UI_X3 + 10
+    y_rep = UI_Y0 + 30
+    offset = 30
+    font_size = 0.7
+
+    label = "Things:"
+    cv2.putText(frame, label, (x_rep,y_rep), cv2.FONT_HERSHEY_SIMPLEX, font_size, (255,255,255), 1)
+    y_rep += offset
+    for i in range(len(classes)):
+        if scores[i] > 0.0:
+            label="  {0:s} {1:.3f}".format(CATEGORY_INDEX[classes[i]]['name'], scores[i])
+            cv2.putText(frame, label, (x_rep,y_rep), cv2.FONT_HERSHEY_SIMPLEX, font_size, (255,255,255), 1)
+            y_rep += offset
+
+    y_rep += offset
+    label = "Stuff:"
+    cv2.putText(frame, label, (x_rep,y_rep), cv2.FONT_HERSHEY_SIMPLEX, font_size, (255,255,255), 1)
+    y_rep += offset
+    unique_stuff = np.unique(image_stuff)
+    for i in range(len(unique_stuff)):
+        if unique_stuff[i] > 91 and unique_stuff[i] < 183:
+            label="  {0:s}".format(CATEGORY_INDEX[unique_stuff[i]]['name'])
+            legend = unique_stuff[i]*np.ones((5,15), np.uint8)
+            legend_color = np.zeros((5, 15,3), dtype='uint8')
+            legend_color[:,:,0] = legend[:,:]
+            legend_color[:,:,1] = legend[:,:]
+            legend_color[:,:,2] = legend[:,:]
+            
+#            disp = np.uint8(255.0 * (frame_color - minValue) / (maxValue - minValue))
+            legend_rgb = cv2.LUT(legend_color, lut)
+            frame[y_rep-5:y_rep+5-5, x_rep+20:x_rep+15+20, 2] = legend_rgb[:,:,0]
+            frame[y_rep-5:y_rep+5-5, x_rep+20:x_rep+15+20, 1] = legend_rgb[:,:,1]
+            frame[y_rep-5:y_rep+5-5, x_rep+20:x_rep+15+20, 0] = legend_rgb[:,:,2]
+            cv2.putText(frame, label, (x_rep+25,y_rep), cv2.FONT_HERSHEY_SIMPLEX, font_size, (255,255,255), 1)
+            y_rep += offset
+
+
+    y_rep += offset
+    label = "Note: Stuff is based on"
+    cv2.putText(frame, label, (x_rep,y_rep), cv2.FONT_HERSHEY_SIMPLEX, font_size, (255,255,255), 1)
+    y_rep += offset
+    label = "      ground truth"
+    cv2.putText(frame, label, (x_rep,y_rep), cv2.FONT_HERSHEY_SIMPLEX, font_size, (255,255,255), 1)
+    y_rep += offset
+    label = "      semantic segmentation."
+    cv2.putText(frame, label, (x_rep,y_rep), cv2.FONT_HERSHEY_SIMPLEX, font_size, (255,255,255), 1)
+    y_rep += offset
+
+    annIds = coco_caps.getAnnIds(imgIds=img['id']);
+    anns = coco_caps.loadAnns(annIds)    
+
+    x_rep = UI_X0 + 10
+    y_rep = UI_Y1 + 30
+    label = "Human generated captions:"
+    cv2.putText(frame, label, (x_rep+5,y_rep), cv2.FONT_HERSHEY_SIMPLEX, font_size, (255,255,255), 1)
+    y_rep += offset
+    for caption in anns:
+        label="  {0:s}".format(caption['caption'])
+        cv2.putText(frame, label, (x_rep+20,y_rep), cv2.FONT_HERSHEY_SIMPLEX, font_size, (255,255,255), 1)
+        y_rep += offset
+
+    
+
     cv2.imshow("ShadowWorld", frame)
     mode = check_keyboard_command(mode)
+#    cv2.waitKey(5000)    
     if mode == "quit":
         break
 
